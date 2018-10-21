9a8c69d3ea995bdc5d53db13c74bce924dc34439
spulgarinv <37346813+spulgarinv@users.noreply.github.com>
2018-10-21 12:38:40 -0500

    Taller11

---
+++ talleres/taller11/Pareja.java
@@ -0,0 +1,11 @@
+package laboratorio5;
+
+/**
+ * @author spulgarinv && jcguerrera
+ */
+public class Pareja
+{
+    public int vertice;
+    public int peso;
+    public Pareja(int v,int p) {vertice = v; peso = p;}
+}
